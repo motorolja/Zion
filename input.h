7e7ea6bbf14462ccc2b122affdd5fd29ef7dfc60
motorolja <rasmusholm957@hotmail.com>
2013-12-21 16:10:37 +0100

    added files for base class input

---
+++ input.h
@@ -0,0 +1,21 @@
+#ifndef INPUT_H
+#define INPUT_H
+
+// Base class for all input 
+class Input
+{
+ private:
+
+ public:
+  Input();
+  ~Input();
+
+ protected:
+
+  /*
+    Shared functions
+  */
+
+};
+
+#endif
